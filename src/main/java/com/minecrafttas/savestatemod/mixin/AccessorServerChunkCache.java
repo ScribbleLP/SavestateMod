aab435b85fb9405a4e8ff202a971c1e39102da2f
Pfannekuchen <gamesupport@mgnet.work>
2022-06-07 23:22:53 +0200

    Less chunkloading, more cache clearing - still doesn't work

---
+++ src/main/java/com/minecrafttas/savestatemod/mixin/AccessorServerChunkCache.java
@@ -0,0 +1,16 @@
+package com.minecrafttas.savestatemod.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+import net.minecraft.server.level.ServerChunkCache;
+import net.minecraft.world.level.storage.DimensionDataStorage;
+
+@Mixin(ServerChunkCache.class)
+public interface AccessorServerChunkCache {
+	@Accessor("dataStorage")
+	public DimensionDataStorage getDimensionDataStorage();
+	@Invoker("clearCache")
+	public void invokeClearCache();
+}
