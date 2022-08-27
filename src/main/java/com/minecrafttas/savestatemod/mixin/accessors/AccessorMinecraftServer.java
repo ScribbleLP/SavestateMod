847bb70a8810017163dbfcbe85e0f468e502722d
Scribble <scribble@minecrafttas.com>
2022-08-27 19:51:14 +0200

    Fixing errors!
    - Running savestating on the server thread
    - Running all tasks inbetween

---
+++ src/main/java/com/minecrafttas/savestatemod/mixin/accessors/AccessorMinecraftServer.java
@@ -0,0 +1,12 @@
+package com.minecrafttas.savestatemod.mixin.accessors;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+import net.minecraft.server.MinecraftServer;
+
+@Mixin(MinecraftServer.class)
+public interface AccessorMinecraftServer {
+	@Invoker("waitUntilNextTick")
+	public void invokeWaitUntilNextTick();
+}
---
+++ src/main/java/com/minecrafttas/savestatemod/savestates/client/LoadstateScreen.java
@@ -0,0 +1,12 @@
+package com.minecrafttas.savestatemod.savestates.client;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.TextComponent;
+
+public class LoadstateScreen extends Screen {
+
+	public LoadstateScreen() {
+		super(new TextComponent("Loadstate Screen"));
+	}
+	
+}
